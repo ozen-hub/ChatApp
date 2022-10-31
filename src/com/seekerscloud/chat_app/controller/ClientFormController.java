579a42c40d344fc57e2ee1b19138afba55f449a2
ozen-hub <info.ozenhub@gmail.com>
2022-10-31 23:38:42 +0530

    10

--- src/com/seekerscloud/chat_app/controller/ClientFormController.java
+++ src/com/seekerscloud/chat_app/controller/ClientFormController.java
@@ -1,4 +1,8 @@
 package com.seekerscloud.chat_app.controller;
 
+import javafx.scene.input.MouseEvent;
+
 public class ClientFormController {
+    public void exitClientOnAction(MouseEvent mouseEvent) {
+    }
 }
