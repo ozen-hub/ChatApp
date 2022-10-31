fd7f05292035e28a16dfbc0c05bde29a64df8865
ozen-hub <info.ozenhub@gmail.com>
2022-10-31 22:32:44 +0530

    2

--- src/com/seekerscloud/chat_app/controller/ServerFormController.java
+++ src/com/seekerscloud/chat_app/controller/ServerFormController.java
@@ -1,5 +1,12 @@
 package com.seekerscloud.chat_app.controller;
 
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
 public class ServerFormController {
 
+    public TextField txtMessageBox;
+
+    public void shutdownServerOnClick(MouseEvent mouseEvent) {
+    }
 }
