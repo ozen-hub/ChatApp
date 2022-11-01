52495b9f02cdaa6f5a2688930745e0268497b9cf
ozen-hub <info.ozenhub@gmail.com>
2022-11-01 19:48:40 +0530

    15

--- src/com/seekerscloud/chat_app/controller/ClientFormController.java
+++ src/com/seekerscloud/chat_app/controller/ClientFormController.java
@@ -1,15 +1,39 @@
 package com.seekerscloud.chat_app.controller;
 
+import com.seekerscloud.chat_app.utill.Client;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+import java.net.Socket;
 
 public class ClientFormController {
+    public VBox vbox_msg;
     String clientName;
+
+    public static VBox senderVBox;
+    public Client client;
 
     public void initialize(){
         System.out.println("initialize");
     }
 
     public void setClientName(String name){
+        new Thread(()->{
+            try {
+                senderVBox = vbox_msg;
+                // cleint name set
+                client = new Client(new Socket("localhost", 5000),
+                        name, vbox_msg);
+                System.out.println("Connected to the server");
+                //=================
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+            }).start();
+
+
+
         clientName=name;
         System.out.println(clientName);
     }
